a9fde6b1d760075432d17fa0682c62aa8f07ef1e
Bumbi <annika.borgesson@gmail.com>
2024-05-24 11:08:24 +0200

    Add serializer and view for displaying list of all dogs #7

--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Dog
+from .serializers import DogSerializer
 
 # Create your views here.
+class DogListView(generics.ListAPIView):
+  """
+  A view for displaying a list of all dogs.
+  """
+  queryset = Dog.objects.all()
+  serializer_class = DogSerializer
