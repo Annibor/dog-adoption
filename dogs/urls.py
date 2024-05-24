fda9ae7d32d08b8926f3fd00a2dc704738631aaa
Bumbi <annika.borgesson@gmail.com>
2024-05-24 11:41:08 +0200

    Add in detail view to show only one dog #7

--- dogs/urls.py
+++ dogs/urls.py
@@ -3,4 +3,5 @@ from dogs import views
 
 urlpatterns = [
   path("dogs/", views.DogListView.as_view()),
+  path('dogs/<int:pk>/', views.DogDetailView.as_view(), name='dog-detail'),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,7 +1,9 @@
 
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 from .models import Dog
 from .serializers import DogSerializer
 
@@ -27,3 +29,17 @@ class DogListView(generics.ListAPIView):
                    'temperament',
                    'good_with_children',
                    'good_with_other_dogs']
+  
+
+class DogDetailView(generics.RetrieveAPIView):
+  """
+  Retrieve the details of a single dog available for adoption.
+  """
+  queryset = Dog.objects.all()
+  serializer_class = DogSerializer
+  lookup_field = 'pk'  # This is the default and can be omitted unless you need a different field.
+
+  def get(self, request, pk):
+    dog = get_object_or_404(Dog, pk=pk)
+    serializer = DogSerializer(dog, context={'request': request})
+    return Response(serializer.data)
