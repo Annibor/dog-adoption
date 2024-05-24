7e3715822217f05b894ea9f1460c9c0794a95530
Bumbi <annika.borgesson@gmail.com>
2024-05-24 10:59:17 +0200

    Add dogs app & add model for dogs #7

--- api_dog_adoption/settings.py
+++ api_dog_adoption/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework',
     
     'profiles',
+    'dogs',
 ]
 
 MIDDLEWARE = [
---
+++ dogs/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+# Create your models here.
+class Dog(models.Model):
+  """
+  Represents a dog available for adoption.
+  
+  Attributes:
+    name (CharField): The dog's name.
+    breed (CharField): The dog's breed.
+    age (IntegerField): The dog's age in years.
+    featured_image (CloudinaryField): An image of the dog, stored externally via Cloudinary.
+    description (TextField): A detailed description of the dog.
+    gender (CharField): The gender of the dog.
+    good_with_children (BooleanField): True if the dog is known to be good with children, False otherwise.
+    good_with_other_dogs (BooleanField): True if the dog is known to be good with other dogs, False otherwise.
+    adoption_status (CharField): The current adoption status of the dog, with choices of 'available', 'pending', or 'adopted'.
+    """
+  name = models.CharField(max_length=80)
+  breed = models.CharField(max_length=80)
+  age = models.IntegerField()
+  featured_image = CloudinaryField('image', default='placeholder')
+  description = models.TextField()
+  gender = models.CharField(max_length=10)
+  good_with_children = models.BooleanField(default=True)
+  good_with_other_dogs = models.BooleanField(default=True)
+  adoption_status = models.CharField(max_length=20, choices=[('available', 'Available'), ('pending', 'Pending'), ('adopted', 'Adopted')], default='available')
+
+
+  class Meta:
+    """
+    Metadata for the Dog model.
+    """
+    verbose_name = "Dog"
+    verbose_name_plural = "Dogs"
+    ordering = ["name"]
+
+  def __str__(self):
+     return self.name
