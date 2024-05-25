7f5bc771d796bbbcab6a0e531ba8905d26aa4d8e
Bumbi <annika.borgesson@gmail.com>
2024-05-25 21:47:34 +0200

    Add views for a list of events & a detailed view for a signle event.#16
     Made sure inly admin can create events

--- events/views.py
+++ events/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import AdoptionEvent, AdoptionEventRegistration
+from .serializers import AdoptionEventSerializer, AdoptionEventRegistrationSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from profiles.models import Profile
+
 
 # Create your views here.
+class AdoptionEventList(generics.ListCreateAPIView):
+  """
+  A view for listing and creating AdoptionEvent objects.
+  Only admins can create AdoptionEvent objects.
+  """
+  queryset = AdoptionEvent.objects.all()
+  serializer_class = AdoptionEventSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
+
+  def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+
+
+class AdoptionEventDetail(generics.RetrieveAPIView):
+  """
+  A view for retrievinga single AdoptionEvent object.
+  """
+  queryset = AdoptionEvent.objects.all()
+  serializer_class = AdoptionEventSerializer
+
+
+
+
+
