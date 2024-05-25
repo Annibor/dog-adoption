d14be1d0f217382e3561e6c97bb534fe9387a85b
Bumbi <annika.borgesson@gmail.com>
2024-05-25 21:01:30 +0200

    Create serializers for event model. #15

---
+++ events/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import AdoptionEvent, AdoptionEventRegistration
+
+
+class AdoptionEventSerializer(serializers.Serializer):
+  """
+  A serializer that serializes AdoptionEvent objects.
+  """
+  class Meta:
+    model = AdoptionEvent
+    fields = ['title', 'description', 'date', 'location', 'created_at']
+
+
+class AdoptionEventRegistrationSerializer(serializers.Serializer):
+  """
+  A serializer that serializes AdoptionEventRegistration objects.
+  """
+  class Meta:
+    model = AdoptionEventRegistration
+    fields = ['event', 'user', 'created_at']
+
