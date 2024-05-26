62906822985701499d83f6f4bbd29de9dfb3e7c6
Bumbi <annika.borgesson@gmail.com>
2024-05-26 21:44:52 +0200

    Add urls for AdoptionApplictions #20

--- dogs/urls.py
+++ dogs/urls.py
@@ -4,4 +4,7 @@ from dogs import views
 urlpatterns = [
   path("dogs/", views.DogListView.as_view()),
   path('dogs/<int:pk>/', views.DogDetailView.as_view(), name='dog-detail'),
+  path
+  path("adoption-applications/", views.AdoptionApplicationListCreateView.as_view()),
+  path("adoption-applications/<int:pk>/", views.AdoptionApplicationDetailView.as_view()),
 ]
