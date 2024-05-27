c0d1231c9d87234e1e86698f40f51696ac049083
Bumbi <annika.borgesson@gmail.com>
2024-05-27 10:50:54 +0200

    Create test for profile model in new test file#23

--- dogs/urls.py
+++ dogs/urls.py
@@ -4,7 +4,6 @@ from dogs import views
 urlpatterns = [
   path("dogs/", views.DogListView.as_view()),
   path('dogs/<int:pk>/', views.DogDetailView.as_view(), name='dog-detail'),
-  path
-  path("adoption-applications/", views.AdoptionApplicationListCreateView.as_view()),
+  path("adoption-applications/", views.AdoptionApplicationListCreatView.as_view()),
   path("adoption-applications/<int:pk>/", views.AdoptionApplicationDetailView.as_view()),
 ]
