46c26e1062be2796877d42ab0032954eee122786
Bumbi <annika.borgesson@gmail.com>
2024-05-30 13:34:58 +0200

    Fixed whitespace, line lenght & indentation to 4 instead of 2 of favorites app files. #26

--- favorites/test_views.py
+++ favorites/test_views.py
@@ -9,46 +9,50 @@ from rest_framework import status
 
 class TestFavoritesView(TestCase):
 
-  def setUp(self):
-    self.client = APIClient()
-    self.user = User.objects.create_user(username='testuser', password='12345')
-    self.profile = Profile.objects.get(owner=self.user)
-    self.client.force_authenticate(user=self.user)
-    self.dog1 = Dog.objects.create(
-      name='Hubbe',
-      breed='Golden Retriever',
-      age=5,
-    )
-    self.dog2 = Dog.objects.create(
-      name='Hejsan',
-      breed='Bulldog',
-      age=3,
-    )
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser',
+                                             password='12345')
+        self.profile = Profile.objects.get(owner=self.user)
+        self.client.force_authenticate(user=self.user)
+        self.dog1 = Dog.objects.create(
+          name='Hubbe',
+          breed='Golden Retriever',
+          age=5,
+        )
+        self.dog2 = Dog.objects.create(
+          name='Hejsan',
+          breed='Bulldog',
+          age=3,
+        )
 
-  def test_can_list_favorites(self):
-    """
-    Test to verify that the favorites can be listed successfully.
-    """
-    response = self.client.get('/favorites/')
-    Favorites.objects.create(user=self.profile, dog=self.dog1)
-    Favorites.objects.create(user=self.profile, dog=self.dog2)
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_can_list_favorites(self):
+        """
+        Test to verify that the favorites can be listed successfully.
+        """
+        response = self.client.get('/favorites/')
+        Favorites.objects.create(user=self.profile, dog=self.dog1)
+        Favorites.objects.create(user=self.profile, dog=self.dog2)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-  def test_logged_in_user_can_add_dog_to_favorites(self):
-    """
-    Test to verify that a logged in user can add a dog to their favorites.
-    """
-    self.client.login(username='testuser', password='12345')
-    response = self.client.post('/favorites/', {'dog': self.dog2.id})
-    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    self.assertTrue(Favorites.objects.filter(user=self.profile, dog=self.dog2).exists())
+    def test_logged_in_user_can_add_dog_to_favorites(self):
+        """
+        Test to verify that a logged in user can add a dog to their favorites.
+        """
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post('/favorites/', {'dog': self.dog2.id})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Favorites.objects.filter(user=self.profile,
+                                                 dog=self.dog2).exists())
 
-  def test_logged_in_user_can_remove_dog_from_favorites(self):
-    """
-    Test to verify that a logged in user can remove a dog from their favorites.
-    """
-    self.client.login(username='testuser', password='12345')
-    Favorites.objects.create(user=self.profile, dog=self.dog1)
-    response = self.client.post('/favorites/', {'dog': self.dog1.id})
-    self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    self.assertFalse(Favorites.objects.filter(user=self.profile, dog=self.dog1).exists())
+    def test_logged_in_user_can_remove_dog_from_favorites(self):
+        """
+        Test to verify that a logged in user can
+        remove a dog from their favorites.
+        """
+        self.client.login(username='testuser', password='12345')
+        Favorites.objects.create(user=self.profile, dog=self.dog1)
+        response = self.client.post('/favorites/', {'dog': self.dog1.id})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Favorites.objects.filter(user=self.profile,
+                                                  dog=self.dog1).exists())
--- favorites/views.py
+++ favorites/views.py
@@ -7,56 +7,56 @@ from dogs.models import Dog
 from .serializers import FavoritesSerializer
 from rest_framework import status
 
+
 # Create your views here.
 class FavoritesView(APIView):
-  """
-  A view for displaying adn updating a user's favorite dogs.
-  """
-  permission_classes = [IsAuthenticated]
-  
-  def get(self, request):
     """
-    Retrieve a list of all favorite dogs for the current user.
+    A view for displaying adn updating a user's favorite dogs.
     """
-    user = request.user.profile
-    favorites = Favorites.objects.filter(user=user)
-    serializer = FavoritesSerializer(favorites, many=True)
-    return Response(serializer.data)
-  
-  def post(self, request):
-    """
-    Create a new favorite or return existing one to avoid duplicates.
-    """
-    user = request.user.profile
-    dog = get_object_or_404(Dog, pk=request.data['dog'])
-    favorite, created = Favorites.objects.get_or_create(user=user, dog=dog)
-    if not created:
-      favorite.delete()
-      return Response(status=status.HTTP_204_NO_CONTENT)
-    return Response(status=status.HTTP_201_CREATED)
+    permission_classes = [IsAuthenticated]
 
-  
+    def get(self, request):
+        """
+        Retrieve a list of all favorite dogs for the current user.
+        """
+        user = request.user.profile
+        favorites = Favorites.objects.filter(user=user)
+        serializer = FavoritesSerializer(favorites, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        """
+        Create a new favorite or return existing one to avoid duplicates.
+        """
+        user = request.user.profile
+        dog = get_object_or_404(Dog, pk=request.data['dog'])
+        favorite, created = Favorites.objects.get_or_create(user=user, dog=dog)
+        if not created:
+            favorite.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_201_CREATED)
+
 
 class FavoriteDetail(APIView):
-  """
-  A view for displaying a single favorite dog.
-  """
-  permission_classes = [IsAuthenticated]
-  
-  def get(self, request, pk):
     """
-    Retrieve a single favorite dog.
+    A view for displaying a single favorite dog.
     """
-    user = request.user.profile
-    favorite = get_object_or_404(Favorites, user=user, dog=pk)
-    serializer = FavoritesSerializer(favorite)
-    return Response(serializer.data)
-  
-  def delete(self, request, pk):
-    """
-    Delete a favorite dog.
-    """
-    user = request.user.profile
-    favorite = get_object_or_404(Favorites, user=user, dog=pk)
-    favorite.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        """
+        Retrieve a single favorite dog.
+        """
+        user = request.user.profile
+        favorite = get_object_or_404(Favorites, user=user, dog=pk)
+        serializer = FavoritesSerializer(favorite)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        """
+        Delete a favorite dog.
+        """
+        user = request.user.profile
+        favorite = get_object_or_404(Favorites, user=user, dog=pk)
+        favorite.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
