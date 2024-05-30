89ee1c0d3ecaf60745fc13ae0da82e5684f11b38
Bumbi <annika.borgesson@gmail.com>
2024-05-30 12:52:13 +0200

    Add in models in admin in evetns app #17

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AdoptionEvent, AdoptionEventRegistration
+
 
 # Register your models here.
+admin.site.register(AdoptionEvent)
+admin.site.register(AdoptionEventRegistration)
