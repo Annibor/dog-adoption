672e10d1854daad1dfa940f9f7e05974e642b9fb
Bumbi <annika.borgesson@gmail.com>
2024-06-04 12:17:11 +0200

    Create pages folder & basic page f iles. Add router for pages in app.jsx #30

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,38 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Dogs from './pages/Dogs'
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PageNotFound from './pages/PageNotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/dogs',
+    element: <Dogs />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '*',
+    element: <PageNotFound />,
+  },
+
+])
+
+
 function App() {
-  return (
-    <div>
-      Hello Vite!
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
