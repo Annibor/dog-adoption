598dfe01bc706445c92ca2ebb418998fd60b422f
Bumbi <annika.borgesson@gmail.com>
2024-06-05 12:10:18 +0200

    Add handle submit &error alerts. #35

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import axios from "axios";
+import { Alert } from 'react-bootstrap';
 
 
 function Register() {
@@ -10,7 +13,11 @@ function Register() {
     confirmPassword: '',
   });
 
-  const { username, email, password, confirmPassword } = FormData;
+  const { username, email, password, confirmPassword } = formData;
+
+  const [error, setError] = useState({});
+
+  const navigate = useNavigate();
 
   const handleChange = (event) => {
     setFormData({
@@ -19,13 +26,41 @@ function Register() {
     });
   };
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/dj-rest-auth/registration/', {
+        username,
+        email,
+        password,
+      });
+
+      if (response.status === 201) {
+        // Registration successful, navigate to another page (e.g., login)
+        navigate('/');
+      } else {
+        console.error('Unexpected response:', response);
+        setError({ non_field_errors: ['Registration failed, please try again!'] });
+      }
+    } catch (err) {
+      // Capture and set specific error messages from the response
+      if (err.response && err.response.data) {
+        const errorData = err.response.data;
+        setError(errorData);
+      } else {
+        setError('An unknown error occurred, please try again!');
+      }
+    }
+  }
+
+
   return (
     <Container className='p-5'>
       <Row className="justify-content-md-center">
         <Col md={6}>
           <h2>Register</h2>
-          <Form>
-          <Form.Group controlId="UserName">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="UserName">
               <Form.Label>Username</Form.Label>
               <Form.Control 
                 type="text" 
@@ -35,6 +70,11 @@ function Register() {
                 onChange={handleChange}
                 required/>
             </Form.Group>
+            {error.username?.map((message, idx) => (
+              <Alert variant='warning' key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="Email">
               <Form.Label>Email address</Form.Label>
@@ -46,6 +86,11 @@ function Register() {
                 onChange={handleChange} 
                 required/>
             </Form.Group>
+            {error.email?.map((message, idx) => (
+              <Alert variant='warning' key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="Password">
               <Form.Label>Password</Form.Label>
@@ -57,17 +102,27 @@ function Register() {
                 onChange={handleChange}
                 />
             </Form.Group>
+            {error.password?.map((message, idx) => (
+              <Alert variant='warning' key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="ConfirmPassword">
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control
                 type="password"
                 placeholder="Confirm Password"
-                name='confirmpassword'
+                name='confirmPassword'
                 value={confirmPassword}
                 onChange={handleChange}
                 />
             </Form.Group>
+            {error.confirmPassword?.map((message, idx) => (
+              <Alert variant='warning' key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Button className='my-4 ' variant="primary" type="submit">
               Register
