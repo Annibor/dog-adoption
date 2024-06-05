e1a4eabc884e31a9280c5ed9c3d0436874b7af10
Bumbi <annika.borgesson@gmail.com>
2024-06-05 21:04:17 +0200

    Add basic structure to profile page #36

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,34 @@
+import { Col, Container, Row } from 'react-bootstrap';
+
 function Profile() {
   return (
     <div>
-      <h1>Profile</h1>
+      <Container>
+        <Row>
+          <Col md={9}>
+            <Row>
+              <Col className='my-4 p-3 bg-warning'>
+                <div>
+                  Here will be liked dogs
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col className='my-4 p-3 bg-danger'>
+                <div>
+                  Here user will be ablw to update user info.
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col md={3} className='bg-primary' >
+            <div>
+              Here will be user profile info
+            </div>
+          </Col>
+        </Row>
+        
+      </Container>
     </div>
   )
 }
