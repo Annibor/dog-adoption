a83eb82ba2064decdb5fa1a1b4a90c113ef89a41
Bumbi <annika.borgesson@gmail.com>
2024-07-06 21:41:25 +0200

    Chabged text in the footer

--- frontend/src/ui/Footer.jsx
+++ frontend/src/ui/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import '../styling/index.css';
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../styling/index.css";
 
 function Footer() {
   return (
@@ -20,8 +20,7 @@ function Footer() {
                   target="_blank"
                   title="Facebook"
                   className="text-light fs-3 m-1"
-                  aria-label="link to facebook"
-                >
+                  aria-label="link to facebook">
                   <i className="fab fa-facebook-f"></i>
                 </Button>
                 <Button
@@ -30,15 +29,17 @@ function Footer() {
                   target="_blank"
                   title="Instagram"
                   className="text-light fs-3 m-1"
-                  aria-label="link to instagram"
-                >
+                  aria-label="link to instagram">
                   <i className="fab fa-instagram"></i>
                 </Button>
               </div>
             </div>
           </Col>
           <Col xs={6} md={4}>
-            <div>Tex ttext </div>
+            <div>
+              Find your perfect furry friend and give them a loving home from
+              our website.{" "}
+            </div>
           </Col>
         </Row>
         <p>&copy; 2024 Dog Adoption. All Rights Reserved.</p>
