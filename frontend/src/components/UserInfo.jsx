9db336774256c17abae9237fd1f0796a43087555
Bumbi <annika.borgesson@gmail.com>
2024-07-21 00:06:38 +0200

    Change the profile page for better understandign #55

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -4,12 +4,9 @@ function UserInfo() {
   return (
     <Card className='user-info-card shadow-sm p-3 mb-5 rounded'>
       <Card.Body>
-        <Card.Title>Username</Card.Title>
+        <Card.Title>Welcome to your profile page!</Card.Title>
         <Card.Text>
-        Here will be userInformation
-        like name
-        number
-        ect
+        Here, you can view and update your personal information, see the dogs you have liked, check your adoption applications, and review your event applications. Use the sections below to navigate through your details and make any necessary updates or changes.
         </Card.Text>
        
       </Card.Body>
