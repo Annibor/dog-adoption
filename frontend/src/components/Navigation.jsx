2e164a6e3282298113b5f5b454f683ae7fc9aa93
Bumbi <annika.borgesson@gmail.com>
2024-07-20 22:47:14 +0200

    Make not logged in visitorsonly see th elogin & register navlink #54

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -27,20 +27,20 @@ export default function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar" />
         <Navbar.Collapse id="basic-navbar">
           <Nav className="m-auto">
-            <NavLink to="/dogs" className="nav-link" >Dogs</NavLink>
-            <NavLink to="/events" className="nav-link">Events</NavLink>
-            <NavLink to="/profile"className="nav-link" >Profile</NavLink>
-            {currentUser ? (
-              <>
-                <Navbar.Text className="nav-link">Welcome!</Navbar.Text>
-                <NavLink onClick={logout} className="nav-link">Logout</NavLink>
-              </>
-            ) : (
-              <>
-                <NavLink to="/login" className="nav-link">Login</NavLink>
-                <NavLink to="/register" className="nav-link">Register</NavLink>
-              </>
-            )}
+          {currentUser ? (
+            <>
+              <NavLink to="/dogs" className="nav-link" >Dogs</NavLink>
+              <NavLink to="/events" className="nav-link">Events</NavLink>
+              <NavLink to="/profile"className="nav-link" >Profile</NavLink>
+              <Navbar.Text className="nav-link">Welcome!</Navbar.Text>
+              <NavLink onClick={logout} className="nav-link">Logout</NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink to="/login" className="nav-link">Login</NavLink>
+              <NavLink to="/register" className="nav-link">Register</NavLink>
+            </>
+          )}
           </Nav>
         </Navbar.Collapse>
       </Container>
