81a178e417708310146969fa0e03fa8f6b1ce5b0
Bumbi <annika.borgesson@gmail.com>
2024-07-23 20:45:48 +0200

    Made dogs page info text get better placement

--- frontend/src/pages/Dogs.jsx
+++ frontend/src/pages/Dogs.jsx
@@ -2,17 +2,16 @@ import DogsList from "../components/DogsList";
 
 function Dogs() {
   return (
-    <div>
-      <div className="">
+    <div className="d-flex flex-column align-items-center">
+      <div className="w-75 text-center">
         <h1 className="text-center pt-3">Dogs</h1>
-        <div className="w-75">
-          <p>This is where you can find all of our dogs that are available for adoption. 
-          <p>Feel free to search or filter between the listed dogs, so you can fain your soulmate!</p>
-          </p>
-        </div>
+        <p>
+          This is where you can find all of our dogs that are available for adoption.
+          <br />
+          Feel free to search or filter between the listed dogs, so you can find your soulmate!
+        </p>
       </div>
-      
-      <div>
+      <div className="w-100">
         <DogsList />
       </div>
     </div>
